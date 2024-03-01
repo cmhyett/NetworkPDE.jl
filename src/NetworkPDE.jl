0002918838cabb9bd61a140fa01aa09ecdc38498
Criston Hyett <cmhyett@math.arizona.edu>
2024-03-01 14:45:59 -0700

    adding exports to module

--- src/NetworkPDE.jl
+++ src/NetworkPDE.jl
@@ -13,6 +13,9 @@ using Symbolics,
 #@register_symbolic Base.floor(T::Type, x)::UInt64
 
 include("graph_interface.jl")
+include("problem_interface.jl")
 #include("proof_of_concept.jl")
+export AbstractNetworkComponent, Edge, Vertex, Network;
+export add_metadata, get_metadata, add_edge, get_edge, get_vertex, num_edges, num_vertices
 
 end
