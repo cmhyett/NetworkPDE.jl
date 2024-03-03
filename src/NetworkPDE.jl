fbe289dcf99e826445c74ff34cfb5d6a428df8ac
Criston Hyett <cmhyett@math.arizona.edu>
2024-03-03 09:20:40 -0700

    formatting

--- src/NetworkPDE.jl
+++ src/NetworkPDE.jl
@@ -15,7 +15,7 @@ using Symbolics,
 include("graph_interface.jl")
 include("problem_interface.jl")
 #include("proof_of_concept.jl")
-export AbstractNetworkComponent, Edge, Vertex, Network;
+export AbstractNetworkComponent, Edge, Vertex, Network
 export add_metadata, get_metadata, add_edge, get_edge, get_vertex, num_edges, num_vertices
 
 end
--- test/network_completion.jl
+++ test/network_completion.jl
@@ -4,25 +4,27 @@ abstract type Pipe <: AbstractNetworkComponent end
 abstract type FluxNode <: AbstractNetworkComponent end
 
 function create_network()
-    e1_params = Dict(:L => 100.0, :λ => 0.11, :D => 0.25, :from_node=>1, :to_node=>2, :type=>Pipe)
-    e2_params = Dict(:L => 50.0, :λ => 0.11, :D => 0.25, :from_node=>2, :to_node=>3, :type=>Pipe)
+    e1_params = Dict(
+        :L => 100.0, :λ => 0.11, :D => 0.25, :from_node => 1, :to_node => 2, :type => Pipe)
+    e2_params = Dict(
+        :L => 50.0, :λ => 0.11, :D => 0.25, :from_node => 2, :to_node => 3, :type => Pipe)
     e1 = Edge(1, e1_params)
     e2 = Edge(2, e2_params)
 
-    v1_params = Dict(:flux => zeros(3), :incident_edges=>[1], :type=>FluxNode);
-    v2_params = Dict(:flux => zeros(3), :incident_edges=>[1,2], :type=>FluxNode);
-    v3_params = Dict(:flux => zeros(3), :incident_edges=>[2], :type=>FluxNode);
+    v1_params = Dict(:flux => zeros(3), :incident_edges => [1], :type => FluxNode)
+    v2_params = Dict(:flux => zeros(3), :incident_edges => [1, 2], :type => FluxNode)
+    v3_params = Dict(:flux => zeros(3), :incident_edges => [2], :type => FluxNode)
     v1 = Vertex(1, v1_params)
     v2 = Vertex(2, v2_params)
     v3 = Vertex(3, v3_params)
 
     g = Network([e1, e2], [v1, v2, v3])
-    return g;
+    return g
 end
 
 @testset "constructing edge PDESystem" begin
     @parameters t
-    net = create_network();
+    net = create_network()
     @test get_metadata(net.edges[1], :type) == Pipe
 
     function instantiate(::Type{Pipe}, comp::AbstractNetworkComponent; name)
@@ -30,19 +32,17 @@ end
         @variables ρ(..) ϕ(..)
         Dt = Differential(t)
         Dx = Differential(x)
-        a = 1.0;
-        eqs = [Dt(ρ(t,x)) + Dx(ϕ(t,x)) ~ 0,
-              Dt(ϕ(t,x)) + a^2 * Dx(ρ(t,x)) ~ 0]
-        ics = [ρ(0,x) ~ 0.0,#comp.initial_conditions[ρ]
-               ϕ(0.0,x) ~ 0.0]#comp.initial_conditions[ϕ]]
+        a = 1.0
+        eqs = [Dt(ρ(t, x)) + Dx(ϕ(t, x)) ~ 0,
+            Dt(ϕ(t, x)) + a^2 * Dx(ρ(t, x)) ~ 0]
+        ics = [ρ(0, x) ~ 0.0,#comp.initial_conditions[ρ]
+            ϕ(0.0, x) ~ 0.0]#comp.initial_conditions[ϕ]]
         bcs = [] #fill in later
         domains = [x in Interval(0, get_metadata(comp, :L))]
-        return PDESystem(eqs, [ics; bcs], domains, [t,x], [ρ(t,x), ϕ(t,x)], name=name);
+        return PDESystem(eqs, [ics; bcs], domains, [t, x], [ρ(t, x), ϕ(t, x)], name = name)
     end
-    @named sys = instantiate(get_metadata(net.edges[1], :type), net.edges[1]);
+    @named sys = instantiate(get_metadata(net.edges[1], :type), net.edges[1])
     @test sys isa PDESystem
 end
 
-@testset "compatibility with units" begin
-
-end
+@testset "compatibility with units" begin end
